51ea04beded23449b8fcabbfeaf284019319af11
Brunao <brunoalmeida0506@gmail.com>
2023-11-22 21:15:56 -0300

    Firt commit

--- exercicio_01/main.c
+++ exercicio_01/main.c
@@ -1,8 +1,21 @@
+
 #include <stdio.h>
 
 int main()
 {
-    printf("Hello World");
+    int cod, quant;
+    float price;
+    printf("Digite o codigo do produto desejado e sua quantiade!: ");
+    scanf("%d", &cod);
+    
+    if(cod == 100) {
+        printf("Cachorro quente selecionado! Digite a quantidade de cachorro quente: ");
+        scanf("%d", &quant);
+        price = 1.20 * quant;
+        printf("Preço total: %f", price);
+    }else{
+        printf("Codigo invalido!")
+    }
 
     return 0;
 }
